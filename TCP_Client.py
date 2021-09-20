b26ffe03c3b47ab1fc8b0db6ce65fde31416c927
karimho007 <bisri.karim@gmail.com>
2021-09-20 12:50:21 -0400

    Files Commited

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = input("Target Website : ")
+target_port = int(input("Target Port : "))
 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
